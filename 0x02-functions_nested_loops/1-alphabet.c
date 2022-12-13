45f4b3cc52bac29c970c4cadd0f9139d6e18c164
dismond5 <omarkhaled201148@gmail.com>
2022-12-13 06:49:42 -0800

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function that prints the alphabet, in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+
+	return (0);
+}
