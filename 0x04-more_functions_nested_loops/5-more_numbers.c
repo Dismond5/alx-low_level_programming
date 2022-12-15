b21c62680956325fe14287f32a3987b4088f0160
dismond5 <omarkhaled201148@gmail.com>
2022-12-15 06:17:59 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 {
 	int i, x;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i >= 10)
+			if (i > 9)
 			{
 				_putchar((i / 10) + '0');
 			}
