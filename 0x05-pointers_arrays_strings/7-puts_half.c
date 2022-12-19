90d7455ca11008cba0dd0ff0fc40ac094c09e970
dismond5 <omarkhaled201148@gmail.com>
2022-12-19 10:03:32 -0800

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,16 +15,16 @@ void puts_half(char *str)
 	{
 		x++;
 	}
-	if (x % 2 ==0)
+	if (x % 2 == 0)
 	{
-		for ( z = x / 2; str[z] != '\0'; z++)
+		for (z = x / 2; str[z] != '\0'; z++)
 		{
 			_putchar(str[z]);
 		}
 	}
 	else if (x % 2)
 	{
-		for ( y = (x - 1) / 2; y < x - 1; y++)
+		for (y = (x - 1) / 2; y < x - 1; y++)
 		{
 			_putchar(str[y + 1]);
 		}
