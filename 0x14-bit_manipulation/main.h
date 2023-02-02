d9391f5def8e7da1414677b6f2988da0bdf24223
dismond5 <omarkhaled201148@gmail.com>
2023-02-02 13:42:15 -0800

    a

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,9 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <stdlib.h>
+#include <stdio.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
